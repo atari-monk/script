70b1a2e52d2c3480c3c8cdf72c84be760db9771e
atari-monk <atari.monk1@gmail.com>
2024-11-11 12:07:51 +0100

    fix bug

--- src/cli_app/commands/log_ai/lib/crud/conversation_crud.py
+++ src/cli_app/commands/log_ai/lib/crud/conversation_crud.py
@@ -1,7 +1,7 @@
 import os
 from libs.crud.crud import CRUD
 from libs.crud.json_storage import JSONStorage
-from commands.log_project.lib.config import DB
+from commands.log_ai.lib.config import DB
 from commands.log_ai.lib.model.conversation import Conversation
 
 class ConversationCRUD(CRUD):
--- src/cli_app/commands/log_ai/lib/crud/dialog_crud.py
+++ src/cli_app/commands/log_ai/lib/crud/dialog_crud.py
@@ -1,7 +1,7 @@
 import os
 from libs.crud.crud import CRUD
 from libs.crud.json_storage import JSONStorage
-from commands.log_project.lib.config import DB
+from commands.log_ai.lib.config import DB
 from commands.log_ai.lib.model.dialog import Dialog
 
 class DialogCRUD(CRUD):
